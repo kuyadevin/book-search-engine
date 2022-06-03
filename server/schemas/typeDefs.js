f71c872f868b32e11f2e9a3c89cc6ae31e2195fc
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-02 22:43:45 -0700

    Add typedefs page

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type: User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: String
+    description: String!
+    image: String
+    title: String!
+    link: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth 
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook! ): User
+    removedBook(bookId: Int!): User
+  }
+`
